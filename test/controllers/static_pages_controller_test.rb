65ef902b97a559d945e3c036114a55c90d7782d9
Nozomu Kotani <b1014239@fun.ac.jp>
2016-12-04 14:58:41 +0000

    list 3.31

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get myapphome" do
     get :myapphome
     assert_response :success
-    assert_select "title", "Myapphome"
+    assert_select "title", "Myapphome | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get home" do
