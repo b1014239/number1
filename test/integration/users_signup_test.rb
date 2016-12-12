7b4189da1f7a653c3778ae706b9f6488fe93e370
Nozomu Kotani <b1014239@fun.ac.jp>
2016-12-12 14:27:06 +0000

    list 7.22 7.21のlog確認

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,10 +5,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, user: { name:  "",
-                               email: "user@invalid",
-                               password:              "foo",
-                               password_confirmation: "bar" }
+      post users_path, user: { name:  "", email: "user@invalid", password: "foo", password_confirmation: "bar" }
     end
     assert_template 'users/new'
   end
